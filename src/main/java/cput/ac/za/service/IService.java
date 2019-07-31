f2bc7cb3d9b13e2a4a6660f7161d3cce19e6d418
Matthew Booysen <MatthewBoo@awards.co.za>
2019-07-31 16:05:55 +0200

    edit

--- src/main/java/cput/ac/za/service/IService.java
+++ src/main/java/cput/ac/za/service/IService.java
@@ -1,7 +1,6 @@
 package cput.ac.za.service;
 
 public interface IService<NameOfRepo, ID> {
-
     NameOfRepo create(NameOfRepo name);
     NameOfRepo read(ID id);
     NameOfRepo update(NameOfRepo name);
