952935cdca85946de58156557f1b3392e314ba76
Matthew Booysen <MatthewBoo@awards.co.za>
2019-08-02 12:50:57 +0200

    edit

--- src/main/java/cput/ac/za/repository/IRepository.java
+++ src/main/java/cput/ac/za/repository/IRepository.java
@@ -2,7 +2,7 @@ package cput.ac.za.repository;
 
 public interface IRepository<NameOfRepo, ID>{
 
-    NameOfRepo create(NameOfRepo name );
+    NameOfRepo create(NameOfRepo name);
     NameOfRepo read(ID id);
     NameOfRepo update(NameOfRepo name);
     void delete(ID id);
