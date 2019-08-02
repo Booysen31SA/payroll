d41e12c8a687ff18b24a1a4a7a570f78f6669141
Matthew Booysen <MatthewBoo@awards.co.za>
2019-08-02 12:13:15 +0200

    Revert "edit"
    This reverts commit f2bc7cb3

--- src/main/java/cput/ac/za/service/IService.java
+++ src/main/java/cput/ac/za/service/IService.java
@@ -1,6 +1,7 @@
 package cput.ac.za.service;
 
 public interface IService<NameOfRepo, ID> {
+
     NameOfRepo create(NameOfRepo name);
     NameOfRepo read(ID id);
     NameOfRepo update(NameOfRepo name);
