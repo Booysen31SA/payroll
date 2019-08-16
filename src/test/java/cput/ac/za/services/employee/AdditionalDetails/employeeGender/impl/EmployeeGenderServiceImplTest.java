317ff53d6ed0c17f0ded5ade5d9ae759a0d21cf0
Matthew Booysen <MatthewBoo@awards.co.za>
2019-08-16 09:28:31 +0200

    Employee gender and Employee race services test

--- src/test/java/cput/ac/za/services/employee/AdditionalDetails/employeeGender/impl/EmployeeGenderServiceImplTest.java
+++ src/test/java/cput/ac/za/services/employee/AdditionalDetails/employeeGender/impl/EmployeeGenderServiceImplTest.java
@@ -1,5 +1,8 @@
 package cput.ac.za.services.employee.AdditionalDetails.employeeGender.impl;
 
+import cput.ac.za.domain.employee.AdditionalDetails.employeeGender.EmployeeGender;
+import cput.ac.za.factory.employee.AdditionalDetails.employeeGender.EmployeeGenderFactory;
+import cput.ac.za.factory.employee.EmployeeFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,12 +10,19 @@ import static org.junit.Assert.*;
 
 public class EmployeeGenderServiceImplTest {
 
+    private EmployeeGenderServiceImpl service;
+    private EmployeeGender employeeGender;
+
     @Before
-    public void setUp(){
+    public void setUp() {
+        service = EmployeeGenderServiceImpl.getService();
+        employeeGender = EmployeeGenderFactory.buildEmployeeGender("216062241", 1);
     }
 
     @Test
     public void getService() {
+        assertNotNull(service);
+        System.out.println(service);
     }
 
     @Test
@@ -21,17 +31,33 @@ public class EmployeeGenderServiceImplTest {
 
     @Test
     public void create() {
+        service.create(employeeGender);
+        assertNotNull(service.read("216062241"));
+        System.out.println("Created\n" + service.read("216062241"));
     }
 
     @Test
     public void read() {
+        assertNotNull(service.read("216062241"));
+        System.out.println("Read\n"+ service.read("216062241"));
     }
 
     @Test
     public void update() {
+        service.create(employeeGender); //Because value gets deleted
+        System.out.println(service.read("216062241"));
+
+        EmployeeGender genderUpdated = EmployeeGenderFactory.buildEmployeeGender("216062241", 2);
+        service.update(genderUpdated);
+        EmployeeGender em = service.read("216062241");
+        assertNotEquals(employeeGender.getCode(), em.getCode());
+        System.out.println("Updated\n"+service.read("216062241"));
     }
 
     @Test
     public void delete() {
+        service.delete("216062241");
+        assertNull(service.read(employeeGender.getEmpID()));
+        System.out.println("Delete\n"+service.read(employeeGender.getEmpID()));
     }
 }
--- src/test/java/cput/ac/za/services/employee/AdditionalDetails/employeeRace/impl/EmployeeRaceServiceImplTest.java
+++ src/test/java/cput/ac/za/services/employee/AdditionalDetails/employeeRace/impl/EmployeeRaceServiceImplTest.java
@@ -1,5 +1,7 @@
 package cput.ac.za.services.employee.AdditionalDetails.employeeRace.impl;
 
+import cput.ac.za.domain.employee.AdditionalDetails.employeeRace.EmployeeRace;
+import cput.ac.za.factory.employee.AdditionalDetails.employeeRace.EmployeeRaceFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,12 +9,19 @@ import static org.junit.Assert.*;
 
 public class EmployeeRaceServiceImplTest {
 
+    private EmployeeRaceServiceImpl service;
+    private EmployeeRace employeeRace;
+
     @Before
-    public void setUp(){
+    public void setUp() {
+        service = EmployeeRaceServiceImpl.getService();
+        employeeRace = EmployeeRaceFactory.buildEmployeeRace("216062241", 1);
     }
 
     @Test
     public void getService() {
+        assertNotNull(service);
+        System.out.println(service);
     }
 
     @Test
@@ -21,17 +30,33 @@ public class EmployeeRaceServiceImplTest {
 
     @Test
     public void create() {
+        service.create(employeeRace);
+        assertNotNull(service.read("216062241"));
+        System.out.println("Created\n" + service.read("216062241"));
     }
 
     @Test
     public void read() {
+        assertNotNull(service.read("216062241"));
+        System.out.println("Read\n"+ service.read("216062241"));
     }
 
     @Test
     public void update() {
+        service.create(employeeRace); //Because value gets deleted
+        System.out.println(service.read("216062241"));
+
+        EmployeeRace genderUpdated = EmployeeRaceFactory.buildEmployeeRace("216062241", 2);
+        service.update(genderUpdated);
+        EmployeeRace em = service.read("216062241");
+        assertNotEquals(employeeRace.getCode(), em.getCode());
+        System.out.println("Updated\n"+service.read("216062241"));
     }
 
     @Test
     public void delete() {
+        service.delete("216062241");
+        assertNull(service.read(employeeRace.getEmpID()));
+        System.out.println("Delete\n"+service.read(employeeRace.getEmpID()));
     }
 }
